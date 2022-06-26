92a212c5e9bba9a78263bec0e4f61eda017a6d04
Bill1907 <a31829660@gamil.com>
2022-06-26 14:54:23 +0900

    apply style to each post

--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 
-// 자신이 원하는 프로필 이미지 링크로 설정해주세요.
 const PROFILE_IMAGE_LINK =
   'https://avatars.githubusercontent.com/u/59524676?v=4'
 
@@ -10,6 +9,10 @@ const ProfileImageWrapper = styled.img`
   height: 120px;
   margin-bottom: 30px;
   border-radius: 50%;
+  @media (max-width: 768px) {
+    width: 80px;
+    height: 80px;
+  }
 `
 
 const ProfileImage: FunctionComponent = () => {
